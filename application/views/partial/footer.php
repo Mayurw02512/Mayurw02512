509fa6d338a5d0fa75295d1fa096dda52d7c1668
Mayurw02512 <155970020+Mayurw02512@users.noreply.github.com>
2024-01-18 17:43:13 +0530

    Update footer.php

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -1,9 +1,4 @@
-<button id="chatbotButton">Open Chatbot</button>
-    <button id="closeChatbotButton" style="display: none;">Close Chatbot</button>
-
-<!-- Chatbot iframe (initially hidden) -->
-<iframe id="chatbotFrame"  src='https://webchat.botframework.com/embed/faq-language-service-bot?s=H7QR6L8cAGw.aJKtOgQnDEfmAPwdpy_z8T0N0hVoF8MNC2ads9LOxUg'   width="300" height="400" frameborder="0" style="display:none;"></iframe>
-<!-- JavaScript to handle button click -->
+<iframe src="https://webchat.botframework.com/embed/mynbot-kblang-bot?s=tJpXvCwD25A.aR8W4a8gMEBG4w_FQz0bXG82IiGvwyN03uG5KedHv4c" style="height: 502px; max-height: 502px;"></iframe>
 <script>
         var chatbotButton = document.getElementById('chatbotButton');
         var closeChatbotButton = document.getElementById('closeChatbotButton');
