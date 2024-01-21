1bef816aa1dc91e566cb34078b78e5f3ae6ca47d
Mayurw02512 <155970020+Mayurw02512@users.noreply.github.com>
2024-01-21 10:46:08 +0530

    Update main.php

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -3,28 +3,7 @@
 	<div class="col-sm"><button id="chatbotButton">Open Chatbot</button></div>
 	<div class="col-sm"><button id="closeChatbotButton" style="display: none;">Close Chatbot</button></div>
 	<div><iframe src="https://webchat.botframework.com/embed/mynbot-kblang-bot?s=tJpXvCwD25A.aR8W4a8gMEBG4w_FQz0bXG82IiGvwyN03uG5KedHv4c" style="height: 502px; max-height: 502px;" class="form-floating"></iframe>
-	</div>	
-  <div class="row text-center">
-      <?php foreach ($product as $value): ?>
-        <div class="col-sm-3">
-          <div class="row p-3"><div class="col-sm">
-            <a href="<?php echo base_url(); ?>index.php/product/<?php echo $value->product_id; ?>"><img src="<?php echo base_url(); ?>assets/product/<?php echo $value->image; ?>" class="img-thumbnail" alt="<?php echo $value->name; ?>"></a>
-          </div></div>
-          <div class="row p-3"><div class="col-sm">
-            <strong><?php echo $value->name; ?></strong></br>
-              Price : <strong><?php echo $value->price; ?></strong>
-          </div></div>
-        </div>
-      <?php endforeach; ?>
-  </div>
-	
-	<div class="col-sm-3">
-	<button id="chatbotButton">Open Chatbot</button></div>
-	<div class="col-sm-3">
-  <button id="closeChatbotButton" style="display: none;">Close Chatbot</button>
-	</div>
-	<div class="row"><div class="col-sm-6">
-		<script>
+	<script>
         var chatbotButton = document.getElementById('chatbotButton');
         var closeChatbotButton = document.getElementById('closeChatbotButton');
         var chatbotFrame = document.getElementById('chatbotFrame');
@@ -46,7 +25,23 @@
             chatbotButton.style.display = 'inline-block';
             closeChatbotButton.style.display = 'none';
         });
-    </script>
+    	</script>
+	</div>	
+  <div class="row text-center">
+      <?php foreach ($product as $value): ?>
+        <div class="col-sm-3">
+          <div class="row p-3"><div class="col-sm">
+            <a href="<?php echo base_url(); ?>index.php/product/<?php echo $value->product_id; ?>"><img src="<?php echo base_url(); ?>assets/product/<?php echo $value->image; ?>" class="img-thumbnail" alt="<?php echo $value->name; ?>"></a>
+          </div></div>
+          <div class="row p-3"><div class="col-sm">
+            <strong><?php echo $value->name; ?></strong></br>
+              Price : <strong><?php echo $value->price; ?></strong>
+          </div></div>
+        </div>
+      <?php endforeach; ?>
+  </div>
+	<div class="row"><div class="col-sm-6">
+		
 	</div>
 	</div>
 </div>
